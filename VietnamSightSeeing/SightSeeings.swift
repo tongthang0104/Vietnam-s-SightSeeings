d94b3b590de1114122e2f07af41c2c3be940bf76
Thang Tong <tongthang0104@gmail.com>
2015-10-14 15:44:26 -0600

    Adding the sight seeing data and theirs description

---
+++ VietnamSightSeeing/SightSeeings.swift
@@ -0,0 +1,26 @@
+//
+//  SightSeeings.swift
+//  VietnamSightSeeing
+//
+//  Created by Thang H Tong on 10/14/15.
+//  Copyright © 2015 Thang. All rights reserved.
+//
+
+import Foundation
+
+class SightSeeings {
+    
+    let name: String
+    let location: String
+    let population: Int
+    let area: Float
+    
+    init(name: String, location: String, population: Int, area: Float) {
+        self.name = name
+        self.location = location
+        self.population = population
+        self.area = area
+    }
+    
+}
+
---
+++ VietnamSightSeeing/SightSeeingsController.swift
@@ -0,0 +1,28 @@
+//
+//  SightSeeingsController.swift
+//  VietnamSightSeeing
+//
+//  Created by Thang H Tong on 10/14/15.
+//  Copyright © 2015 Thang. All rights reserved.
+//
+
+import Foundation
+
+class SightSeeingsController {
+  
+    // Create the sightseeings
+    static var sightSeeing: [SightSeeings] {
+        
+        
+        let sapa = SightSeeings(name: "SaPa Mountain", location: "Northest Region of Vietnam", population: 8975, area: 24.02)
+        let haNoi = SightSeeings(name: "Ha Noi Capitol", location: "North Region of Vietnam", population: 7087700, area: 3324.5)
+        let haLongBay = SightSeeings(name: "Ha Long Bay", location: "In Capitol Ha Noi", population: 221580, area: 271.95)
+        let hoiAn = SightSeeings(name: "Hoi An City", location: "Middle Region of Vietnam", population: 121716, area: 60)
+        let nhaTrang = SightSeeings (name: "Nha Trang City", location: "South Central of Vietnam", population: 400000, area: 252.6)
+        let saigon = SightSeeings(name: "Saigon City", location: "South Region of Vietnam", population: 7981900 , area: 2095.5)
+        let phuQuoc = SightSeeings(name: "Phu Quoc", location: "Southest Region of Vietnam", population: 103000, area: 593.05)
+        let caMau = SightSeeings(name: "Ca Mau City", location: "Southest Region of Vietnam", population: 2058 , area: 1200000)
+        
+        return [sapa, haNoi, haLongBay, hoiAn, nhaTrang, saigon, phuQuoc, caMau]
+    }
+}
