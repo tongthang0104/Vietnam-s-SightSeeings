f8618091e5ce5261e27d152bdb41fefaaedae575
Thang Tong <tongthang0104@gmail.com>
2015-10-14 15:25:05 -0600

    Initial Commit

---
+++ VietnamSightSeeing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VietnamSightSeeing
+//
+//  Created by Thang H Tong on 10/14/15.
+//  Copyright © 2015 Thang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
